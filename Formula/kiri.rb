aad897ad509cfa5cd2bb87b8a95e645374cf81d7
高春晖 <18220699480@163.com>
2026-05-23 02:34:13 +0800

    chore: 更新 Kiri formula 到 0.1.6
    - 将 Homebrew formula 版本升级到 0.1.6
    - 更新 arm64 和 x86_64 macOS release tarball 的真实 sha256

--- Formula/kiri.rb
+++ Formula/kiri.rb
@@ -1,16 +1,16 @@
 class Kiri < Formula
   desc "Inspect local development listening ports"
   homepage "https://github.com/gaossr/kiri"
-  version "0.1.5"
+  version "0.1.6"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/gaossr/kiri/releases/download/v#{version}/kiri-aarch64-apple-darwin.tar.gz"
-      sha256 "b48248ccc91b83c8ef7018ffc0aebabede63ac5411501d9679d6377554df62c9"
+      sha256 "e23ab4e5225d0ec4900b921d40d62e50b20e903a5c374dc9da9978b2ad3d54d9"
     else
       url "https://github.com/gaossr/kiri/releases/download/v#{version}/kiri-x86_64-apple-darwin.tar.gz"
-      sha256 "60e94c0f0d3cc106a82aa21f7c74f20e8a545bea972dbaba2dcdcc2609f3cd49"
+      sha256 "bc415e52d886c8c7ded57549f17798fd473864de488d2f29a17edeff33bd4d5c"
     end
   end
 
