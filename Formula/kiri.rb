333b1a8ac7de79bf7275e6b886e9007762145e96
高春晖 <18220699480@163.com>
2026-05-23 02:10:12 +0800

    chore: 更新 Kiri formula 到 0.1.4
    - 将 formula version 更新为 0.1.4
    - 替换 aarch64 和 x86_64 macOS release artifact 的 sha256

--- Formula/kiri.rb
+++ Formula/kiri.rb
@@ -1,16 +1,16 @@
 class Kiri < Formula
   desc "Inspect local development listening ports"
   homepage "https://github.com/gaossr/kiri"
-  version "0.1.3"
+  version "0.1.4"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/gaossr/kiri/releases/download/v#{version}/kiri-aarch64-apple-darwin.tar.gz"
-      sha256 "cb8cb42a6c25cf1d1e771e66ae8e58c7792597f9fa4b01c328149d5f6c5cc986"
+      sha256 "d45f95206018cd090cbcceefbd0022d8485dee60d63e73ae5d3caa6525a43197"
     else
       url "https://github.com/gaossr/kiri/releases/download/v#{version}/kiri-x86_64-apple-darwin.tar.gz"
-      sha256 "e6dc33d95f9582329da81a93e2237f14a371e2d5e8dda0852f32194492667af5"
+      sha256 "981741cb8ce16a62906ade06b80f066d71c306c4edee43a003a300f27858ab1d"
     end
   end
 
