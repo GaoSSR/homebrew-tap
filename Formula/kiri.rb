a3dca3406aa9eccf35d9b1affbf00fcdfc0253f3
高春晖 <18220699480@163.com>
2026-05-23 16:22:18 +0800

    chore: 去掉 Kiri formula 冗余版本字段

--- Formula/kiri.rb
+++ Formula/kiri.rb
@@ -1,15 +1,14 @@
 class Kiri < Formula
   desc "Inspect local development listening ports"
   homepage "https://github.com/gaossr/kiri"
-  version "0.1.6"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gaossr/kiri/releases/download/v#{version}/kiri-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/gaossr/kiri/releases/download/v0.1.6/kiri-aarch64-apple-darwin.tar.gz"
       sha256 "e23ab4e5225d0ec4900b921d40d62e50b20e903a5c374dc9da9978b2ad3d54d9"
     else
-      url "https://github.com/gaossr/kiri/releases/download/v#{version}/kiri-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/gaossr/kiri/releases/download/v0.1.6/kiri-x86_64-apple-darwin.tar.gz"
       sha256 "bc415e52d886c8c7ded57549f17798fd473864de488d2f29a17edeff33bd4d5c"
     end
   end
