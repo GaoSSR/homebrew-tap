dd0bb149abf4fa53d766552164ef2a95aa745985
高春晖 <18220699480@163.com>
2026-05-23 18:40:33 +0800

    chore: 更新 Kiri formula 到 0.1.10

--- Formula/kiri.rb
+++ Formula/kiri.rb
@@ -5,11 +5,11 @@ class Kiri < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gaossr/kiri/releases/download/v0.1.9/kiri-aarch64-apple-darwin.tar.gz"
-      sha256 "9d0b533d8f87454078f1da35809e39df53a8f8c59d730bd50428283f3c513e19"
+      url "https://github.com/gaossr/kiri/releases/download/v0.1.10/kiri-aarch64-apple-darwin.tar.gz"
+      sha256 "b58626529e069d93024e0b388187a10175c96a9239c9ffb0234efc39780e312e"
     else
-      url "https://github.com/gaossr/kiri/releases/download/v0.1.9/kiri-x86_64-apple-darwin.tar.gz"
-      sha256 "2a3e838ac4e2dd6ee8856c1984b09af0a9185503049dbd8176d2253fc35303da"
+      url "https://github.com/gaossr/kiri/releases/download/v0.1.10/kiri-x86_64-apple-darwin.tar.gz"
+      sha256 "b63d4b76292e21c3a6d5a8e6108ee1da743f4ef29df9ef8b76ff7c6dc24ce1a2"
     end
   end
 
